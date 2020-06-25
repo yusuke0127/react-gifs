684ea952fbbc360fa1b0e9a99667a28571b2eeab
yusuke ishida <ishidayusuke01@gmail.com>
2020-06-25 15:26:08 +0900

    Refactored some codes

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -5,10 +5,20 @@ class Gif extends Component {
   handleClick = () => {
     // console.log(this.props);
     // console.log(this.props.selectGif);
-    this.props.selectGif(this.props.id);
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.id !== this.props.id;
   }
 
   render() {
+    if (!this.props.id) {
+      return null;
+    }
+
     const src = `https://media.giphy.com/media/${this.props.id}/giphy.gif`;
     return (
 
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -2,16 +2,40 @@ import React, { Component } from 'react';
 
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ""
+    }
+  }
+
   handleUpdate = (event) => {
     // console.log(this.props);
     // console.log(this.props.searchFunction);
+    this.setState({
+      term: event.target.value
+    })
     this.props.searchFunction(event.target.value);
   }
 
+  componentWillMount() {
+    console.log("SEARCH BAR WILL MOUND");
+  }
+
+  componentDidMount() {
+    console.log("SEARCH DID MOUNT");
+  }
+
+
   render() {
+    console.log("SEARCH BAR RENDER");
     return (
 
-      <input type="text" className="form-control form-search"
+      <input
+        value={this.state.term}
+        type="text"
+        className="form-control form-search"
         onChange={this.handleUpdate}
       />
     );
